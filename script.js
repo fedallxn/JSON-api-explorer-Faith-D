85a14f9cd3929128035cfc5cf849f6beaed718bf
fedallxn <faithe.dall@gmail.com>
2026-02-03 18:15:55 -0600

    I used fetch to "get" JSON information from the public API we were assigned. I added an event listener to the fetch button so that it retrieves just the posts titles and bodies. I also added an event listner to the submit button so that when a user clicks it it will simulate it being posted (will show up in the console but not in the actial JSON since it;s simulated). This used the same fetch function but changing the mthos to POST instead if the default, GET. I, unfortuneately didn't have enough time to do the extenstions, like simulating deleting a post or being able to filter through them.

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-// so much empty
+//ALL MY HTML IDs!! and a fecthPromise (didn't know if that was applicable here???)
+const fetchPromise = fetch("https://jsonplaceholder.typicode.com/posts");
+const posts = document.getElementById("postList");
+const fetchBtn = document.getElementById("fetchButton");
+const formData = document.getElementById("postForm");
+const successMssg = document.getElementById("formSuccess");
+const formErr = document.getElementById("formError");
+const fetchErr = document.getElementById("error");
+
+//GET endpoint -- retrieves list of posts by title and body
+fetchPromise.then(function(response) {
+    return response.json();
+})
+.then(function (json) {
+    console.log(json);
+
+        fetchBtn.addEventListener("click", () => {
+        let renderedPosts = '';
+        //Only wanted to access the title and body of each post
+        json.forEach(({title, body}, index) => {
+            renderedPosts += `
+            <li> <b>Post ${index + 1} -- Title:</b> <i>${title}</i> </li>
+            <li> ${body} </li>`
+        });
+    posts.innerHTML = renderedPosts;
+    })
+})
+.catch(err => {
+    //displays a message if there was an issue while tring to fetch data
+    fetchErr.innerHTML = "Error while fetching data..."
+    console.error("Oops: ", err);
+})
+
+//POST endpoint -- mock submitting a new post! Shows in console but can't be retrieved by "Fetch Posts" btn
+formData.addEventListener("submit", (event) => {
+    event.preventDefault(); //prevent my page from automatically reloading
+
+    const formTitle = document.getElementById("titleInput").value;
+    const formBody = document.getElementById("bodyInput").value;
+
+    //lil loading message! The fetching is rather fast so it only shows up for a split second
+    successMssg.innerHTML = "Sending in progress..."
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+            title: formTitle,
+            body: formBody,
+            userID: 1,
+        }),
+        headers: {
+            "content-type": "application/json; charset=UTF-8",
+        },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        //successfully sent!
+        successMssg.innerHTML = "Post sent successfully! Thank you!";
+        formData.reset(); //clears the input fields in the form instead of manually resetting each field
+    })
+    .catch(err => {
+        //displays an error if there was an issue with sending the post
+        formErr.innerHTML = "Error while sending post. Try again.";
+        console.error("Opps: ", err);
+    })
+});
